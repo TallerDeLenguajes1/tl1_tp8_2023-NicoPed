bb8538ebf3690382bb547c9b19168a3f6a31f597
NicoPed <nikopedraza10@gmail.com>
2023-06-08 18:23:49 -0300

    Initial commit

---
+++ Listas.cs
@@ -0,0 +1,22 @@
+//crear una lase lista
+//addrange concatenear una lista a otra
+//remove manda la tarea osea el objeto a sacar
+//removeAt Toma un indice
+//son como quitar
+//find tarea = pendiente.Find(t => t.Id == 3)
+
+namespace Tareas;
+
+public class Tarea
+{
+    private int duracion;
+    private int tareaId;
+    private string? descripcion;
+    public int Duracion { get => duracion; set => duracion = value; }
+    public int TareaId { get => tareaId; set => tareaId = value; }
+    public string? Descripcion { get => descripcion; set => descripcion = value; }
+    
+    public Tarea(){
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Tareas;
+namespace ToDo // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        const int cantTarea = 5;
+        static void Main(string[] args)
+        {
+            var pendiente = new List<Tarea>();
+            var realizadas = new List<Tarea>();
+            cargarAleatoriamente(pendiente);
+            
+        }
+        private static void cargarAleatoriamente(List<Tarea> Lista){
+            var nuevaTarea = new Tarea();
+            for (int i = 0; i < cantTarea; i++)
+            {
+                nuevaTarea.TareaId = i;
+                nuevaTarea.Duracion = i*3+5;
+                nuevaTarea.Descripcion = "Desripicion"+ i;
+                Lista.Insert(0,nuevaTarea);            
+            }
+        }
+    }
+}
