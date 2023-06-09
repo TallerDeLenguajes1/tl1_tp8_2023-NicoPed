6452b65271d23e0d42cfc01fc03d1e6fe868e3e5
NicoPed <nikopedraza10@gmail.com>
2023-06-09 13:49:54 -0300

    Realizado primera parte del punto 2, tengo un error al hacer la otra parte que no entiendo

--- Program.cs
+++ Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            
+            string rutaDeCarpeta = @"C:\Users\Nico Pedraza\Documents\TPs_TLI\carpetaPrueba";
+            if (Directory.Exists(rutaDeCarpeta))
+            {
+                List<string> archivos = Directory.GetFiles(rutaDeCarpeta).ToList();
+                foreach (var archivo in archivos)
+                {
+                    Console.WriteLine(archivo);
+                }
+            string rutaArchivoCSV = Path.Combine(rutaDeCarpeta, "index.csv");
+            if (!File.Exists(rutaArchivoCSV))
+            {
+                File.Create(rutaArchivoCSV);
+            }
+            using (StreamWriter writer = new StreamWriter(rutaArchivoCSV)){
+                writer.WriteLine("indice;nombre;extension");
+                foreach (var archivo in archivos)
+                {
+                    var infoArchivo = new FileInfo(archivo);
+                    writer.WriteLine($"{infoArchivo.Directory};{infoArchivo.Name};{infoArchivo.Extension}");
+
+                }
+            }
+            }
+            else
+            {
+                Console.WriteLine("No existe la susodicha carpeta");
+            }
         }
         
     }
